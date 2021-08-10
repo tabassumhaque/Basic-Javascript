47f18fd0a435a164e5ab175593869d8a67484515
tabassumhaque <tabassum.haque31@gmail.com>
2021-08-10 22:07:33 +0600

    Add existing file

---
+++ conversion.js
@@ -0,0 +1,19 @@
+// Unit Convert inch to feet
+
+// function inchToFeet(inches){
+//     const feet = inches / 12;
+//     return feet;
+// }
+// const myInches = inchToFeet(24);
+// console.log(myInches);
+
+// console.log(inchToFeet(48));
+
+
+// Miles to kilometers
+
+// function milesToKm(mile){
+//     const kilometers = mile * 0.621;
+//     return kilometers;
+// }
+// console.log(milesToKm(10));
