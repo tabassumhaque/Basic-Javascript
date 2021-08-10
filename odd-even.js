3e29f5736f42473b413728a2b5bc55f7be2c3f4b
tabassumhaque <tabassum.haque31@gmail.com>
2021-08-10 22:48:54 +0600

    Add again

---
+++ odd-even.js
@@ -0,0 +1,24 @@
+// Check Even and odd numbers using Functions
+
+// Check Even Numbers
+// function checkEvenNumber(number){
+//     if(number % 2 == 0){
+//         return 'Even';
+//     }
+//     else{
+//         return 'Odd'
+//     }
+// }
+// console.log(checkEvenNumber(556));
+
+
+// Check Odd Numbers
+function checkOddNumber(number){
+    if(number % 2 == 1){
+        return 'Odd';
+    }
+    else{
+        return 'Even'
+    }
+}
+console.log(checkOddNumber(561));
