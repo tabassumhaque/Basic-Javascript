82a1f728eae29aedde36cc0e7f6e217ff953a72d
tabassumhaque <tabassum.haque31@gmail.com>
2021-08-19 12:01:35 +0600

    new one

---
+++ leapyear.js
@@ -0,0 +1,12 @@
+// Check whether a year is leap years or not
+// function checkLeapYear(year){
+//     if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+//         return 'leap year';
+//     }
+//     else{
+//         return 'is not a leap year';
+//     }
+// }
+
+// const myYear = checkLeapYear(2061);
+// console.log(myYear);
